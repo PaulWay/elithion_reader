e845a499430c601edb2c52876723cf9051798b86
Paul Wayper <paulway@mabula.net>
2017-11-21 21:29:03 +1100

    Add read delay argument, minor syntactic fixes, move output open to read_to_file

--- elith_reader.py
+++ elith_reader.py
@@ -26,20 +26,29 @@ line_re = re.compile(
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    '--device', '-d', type=str, nargs='?', default=device_name, action='store',
+    '--device', '-d', type=str, default=device_name, action='store',
     help='the device to read from'
 )
 parser.add_argument(
-    '--outdir', '-o', type=str, nargs='?', default=output_dir, action='store',
+    '--outdir', '-o', type=str, default=output_dir, action='store',
     help='the base directory to write output to'
 )
+parser.add_argument(
+    '--read-delay', '-r', type=int, action='store', dest='readdelay', default=read_delay
+    help='the time between finishing one read and starting the next'
+)
 args = parser.parse_args()
-if args.device:
-    device_name = args.device
-if args.outdir:
-    output_dir = args.outdir
+device_name = args.device
+output_dir = args.outdir
+read_delay = args.readdelay
 
 def read_to_file(dev, outfile):
+    outfile = open(
+        output_dir + output_prefix + 
+        datetime.now().strftime('%Y%m%d-%H%M%S') + output_suffix,
+        'w', False  # for no buffering
+    )
+
     while True:
         dev.write('v')
         buf = ''
@@ -50,17 +59,18 @@ def read_to_file(dev, outfile):
             match = line_re.search(buf)
             tries += 1
         
-        if (tries == maxtries):
+        if (tries == max_tries):
             print "Got no matching data, retrying command"
         else:
-            outfile.write(','.join(
+            outfile.write(','.join([
                 datetime.now().isoformat(),
                 match.group('type'),
                 match.group('len'),
                 match.group('data'),
                 match.group('csum'),
-            ), "\n")
+            ]) + "\n")
             buf = match.group('rest')
+            print "got data at", datetime.now().isoformat(), "\r"
         
         time.sleep(read_delay)
 
@@ -74,16 +84,8 @@ while True:
     dev = serial.Serial(device_name, 57600, timeout=5, xonxoff=False, rtscts=False)
     print "opened serial port", device_name
     
-    outfile = open(
-        output_dir + output_prefix + 
-        datetime.now().strftime('%Y%m%d-%H%M%S') + output_suffix,
-        'w'
-    )
-
     try:
-        read_to_file(dev, outfile)
+        read_to_file(dev)
     except serial.serialutil.SerialException:
         print "Got exception, aborting read"
         dev = None
-    
-    outfile.close()
